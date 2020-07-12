55b84395e4b5bf3fc8689debca375ae0c62714aa
Abdvkh <e-address@bk.ru>
2020-07-12 17:56:35 +0500

    Blog API all features with Auth-es

---
+++ posts/views.py
@@ -0,0 +1,17 @@
+from rest_framework import generics,\
+						   permissions
+
+from .models import Post
+from .permissions import IsAuthorOrReadOnly
+from .serializers import PostSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+	queryset = Post.objects.all()
+	serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+	permission_classes = (IsAuthorOrReadOnly, permissions.IsAuthenticated, 	)
+	queryset = Post
+	serializer_class = PostSerializer
